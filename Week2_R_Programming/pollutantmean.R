75c5b093d45eea6970ce4788507e3cf5389104c9
gmassacc <11818620+gmassacc@users.noreply.github.com>
2019-01-17 17:48:18 -0500

    Update pollutantmean.R

--- Week2_R_Programming/pollutantmean.R
+++ Week2_R_Programming/pollutantmean.R
@@ -1,6 +1,6 @@
 pollutantmean <- function(directory, pollutant, id=1:332){#args: [1]directory name [2] pollutant whose mean we want [3] ids of all csv files to be imported
   #set working directory for these files
-  setwd("/Users/guidomassaccesi/Documents/Documents/Coursera/R Programming/Week 2/")
+  setwd("/.../Coursera/R Programming/Week 2/")
   #create a list of files in the directory folder
   file_names <- list.files(directory, full.names = TRUE)
   #create empty data frame
